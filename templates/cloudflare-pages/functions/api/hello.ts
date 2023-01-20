0df6b92251ea7c8c6f107d2d3b2b2bdfd5b244f9
Yusuke Wada <yusuke@kamawada.com>
2023-01-21 01:36:22 +0900

    feat: add cloudflare-pages template

---
+++ templates/cloudflare-pages/functions/api/hello.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/cloudflare-pages'
+
+const app = new Hono()
+
+app.get((c) => {
+  return c.json({
+    message: 'Hello from Hono!',
+  })
+})
+
+export const onRequestGet = handle(app)
