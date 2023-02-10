d638ee0da42b075d4f3c321d25e83d56f519557f
Yusuke Wada <yusuke@kamawada.com>
2023-02-11 06:56:58 +0900

    chore(deno): use `npm:hono`

--- templates/deno/hello.ts
+++ templates/deno/hello.ts
@@ -1,5 +1,5 @@
 import { serve } from 'https://deno.land/std@0.167.0/http/server.ts'
-import { Hono } from 'https://deno.land/x/hono@v2.7.2/mod.ts'
+import { Hono } from 'npm:hono@2.7.7'
 
 const app = new Hono()
 
