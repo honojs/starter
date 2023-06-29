744e01fe0ef1fac24cd2dcfa6766ac4f5c68210e
Yusuke Wada <yusuke@cloudflare.com>
2023-06-29 20:16:18 +0900

    bump up Hono

--- templates/deno/hello.ts
+++ templates/deno/hello.ts
@@ -1,5 +1,5 @@
 import { serve } from 'https://deno.land/std@0.167.0/http/server.ts'
-import { Hono } from 'https://deno.land/x/hono@v3.2.6/mod.ts'
+import { Hono } from 'https://deno.land/x/hono@v3.2.7/mod.ts'
 
 const app = new Hono()
 
