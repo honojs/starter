3a0ee7c2b5c56ad8f321c816f5ce10d4526c6c8f
Yusuke Wada <yusuke@cloudflare.com>
2023-08-08 11:37:24 +0900

    simplify

--- templates/netlify/netlify/edge-functions/index.ts
+++ templates/netlify/netlify/edge-functions/index.ts
@@ -1,13 +1,10 @@
+import { handle } from 'https://deno.land/x/hono@v3.4.1/adapter/netlify/mod.ts'
 import { Hono } from 'https://deno.land/x/hono@v3.4.1/mod.ts'
-import { prettyJSON } from 'https://deno.land/x/hono@v3.4.1/middleware.ts'
-import { handle, type Env } from 'https://deno.land/x/hono@v3.4.1/adapter/netlify/mod.ts'
 
-const app = new Hono<Env>()
+const app = new Hono()
 
-app.get('/country', prettyJSON(), (c) =>
-  c.json({
-    'You are in': c.env.context.geo.country?.name
-  })
-)
+app.get('/country', (c) => {
+  return c.text('Hello Hono!')
+})
 
 export default handle(app)
