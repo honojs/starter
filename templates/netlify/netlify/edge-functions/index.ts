a29f0d62bef6ca24c3dc4c1be8f0b7696f491101
Yusuke Wada <yusuke@kamawada.com>
2023-08-08 11:37:46 +0900

    add a netlify template (#5)
    * add a netlify template
    
    * simplify

---
+++ templates/netlify/netlify/edge-functions/index.ts
@@ -0,0 +1,10 @@
+import { handle } from 'https://deno.land/x/hono@v3.4.1/adapter/netlify/mod.ts'
+import { Hono } from 'https://deno.land/x/hono@v3.4.1/mod.ts'
+
+const app = new Hono()
+
+app.get('/country', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default handle(app)
