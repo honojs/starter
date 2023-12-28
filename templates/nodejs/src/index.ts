088335f5e8f8507ab2d469a459b6933f09558f0c
Yusuke Wada <yusuke@kamawada.com>
2023-12-29 04:36:57 +0900

    show starting message for Node.js

--- templates/nodejs/src/index.ts
+++ templates/nodejs/src/index.ts
@@ -4,4 +4,10 @@ import { Hono } from 'hono'
 const app = new Hono()
 app.get('/', (c) => c.text('Hello Hono!'))
 
-serve(app)
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
