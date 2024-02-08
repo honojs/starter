cc3969b11423d4f2560edc49afa429d907eefd84
Yusuke Wada <yusuke@kamawada.com>
2024-02-09 01:04:43 +0900

    wip

---
+++ templates/x-basic/vite.config.ts
@@ -0,0 +1,16 @@
+import pages from '@hono/vite-cloudflare-pages'
+import honox from 'honox/vite'
+import client from 'honox/vite/client'
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      plugins: [client()]
+    }
+  } else {
+    return {
+      plugins: [honox(), pages()]
+    }
+  }
+})
