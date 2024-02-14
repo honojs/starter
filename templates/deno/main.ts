cac7995f712594f773a9f1b50396454f19e78b2c
Yusuke Wada <yusuke@kamawada.com>
2024-02-14 18:09:14 +0900

    bump hono

--- templates/deno/main.ts
+++ templates/deno/main.ts
@@ -1,4 +1,4 @@
-import { Hono } from 'https://deno.land/x/hono@v4.0.1/mod.ts'
+import { Hono } from 'https://deno.land/x/hono@v4.0.2/mod.ts'
 
 const app = new Hono()
 
--- templates/netlify/netlify/edge-functions/index.ts
+++ templates/netlify/netlify/edge-functions/index.ts
@@ -1,5 +1,5 @@
-import { handle } from 'https://deno.land/x/hono@v4.0.1/adapter/netlify/mod.ts'
-import { Hono } from 'https://deno.land/x/hono@v4.0.1/mod.ts'
+import { handle } from 'https://deno.land/x/hono@v4.0.2/adapter/netlify/mod.ts'
+import { Hono } from 'https://deno.land/x/hono@v4.0.2/mod.ts'
 
 const app = new Hono()
 
