280947d2b01b6fd6959886028aa0cf94937d8e36
Yusuke Wada <yusuke@kamawada.com>
2024-02-14 17:56:10 +0900

    remove docType option since now, true is as default

--- templates/cloudflare-pages/src/renderer.tsx
+++ templates/cloudflare-pages/src/renderer.tsx
@@ -1,18 +1,13 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
 
-export const renderer = jsxRenderer(
-  ({ children, title }) => {
-    return (
-      <html>
-        <head>
-          <link href="/static/style.css" rel="stylesheet" />
-          <title>{title}</title>
-        </head>
-        <body>{children}</body>
-      </html>
-    )
-  },
-  {
-    docType: true
-  }
-)
+export const renderer = jsxRenderer(({ children, title }) => {
+  return (
+    <html>
+      <head>
+        <link href="/static/style.css" rel="stylesheet" />
+        <title>{title}</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+})
