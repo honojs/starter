3780e2c2e5809cdd2f30a52a15b83151e8d77ff9
Yusuke Wada <yusuke@kamawada.com>
2024-06-26 11:12:06 +0900

    update `_error.tsx` for `x-basic`

--- templates/x-basic/app/routes/_error.tsx
+++ templates/x-basic/app/routes/_error.tsx
@@ -1,6 +1,9 @@
 import type { ErrorHandler } from 'hono'
 
 const handler: ErrorHandler = (e, c) => {
+  if ('getResponse' in e) {
+    return e.getResponse()
+  }
   console.error(e.message)
   c.status(500)
   return c.render('Internal Server Error')
