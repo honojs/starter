ba7ede1ba8300925c1dd3ee02ad643cb241763b0
Yusuke Wada <yusuke@kamawada.com>
2024-07-18 21:40:04 +0900

    update the `cloudflare-pages` template

--- templates/cloudflare-pages/src/renderer.tsx
+++ templates/cloudflare-pages/src/renderer.tsx
@@ -1,11 +1,10 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
 
-export const renderer = jsxRenderer(({ children, title }) => {
+export const renderer = jsxRenderer(({ children }) => {
   return (
     <html>
       <head>
         <link href="/static/style.css" rel="stylesheet" />
-        <title>{title}</title>
       </head>
       <body>{children}</body>
     </html>
