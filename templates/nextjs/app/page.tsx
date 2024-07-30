4be11184567d1d0a47da62ba4f377255fb13d78e
Sadiq <riomahod@gmail.com>
2024-07-30 15:55:15 +0300

    update format templates

--- templates/nextjs/app/page.tsx
+++ templates/nextjs/app/page.tsx
@@ -1,19 +1,19 @@
-"use client";
-import { useEffect, useState } from "react";
+'use client'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-    const [message, setMessage] = useState();
+  const [message, setMessage] = useState()
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch("/api/hello");
-            const { message } = await res.json();
-            setMessage(message);
-        };
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/api/hello')
+      const { message } = await res.json()
+      setMessage(message)
+    }
+    fetchData()
+  }, [])
 
-    if (!message) return <p>Loading...</p>;
+  if (!message) return <p>Loading...</p>
 
-    return <p>{message}</p>;
+  return <p>{message}</p>
 }
