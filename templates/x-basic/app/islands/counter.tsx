52fb52d7920628c4dbe6f72b21cf8538669a3449
Yusuke Wada <yusuke@kamawada.com>
2025-02-28 12:34:46 +0900

    use Tailwind CSS for `x-basic`

--- templates/x-basic/app/islands/counter.tsx
+++ templates/x-basic/app/islands/counter.tsx
@@ -4,8 +4,13 @@ export default function Counter() {
   const [count, setCount] = useState(0)
   return (
     <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <p class="py-2 text-2xl">{count}</p>
+      <button
+        class="px-4 py-2 bg-orange-400 text-white rounded cursor-pointer"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </button>
     </div>
   )
 }
