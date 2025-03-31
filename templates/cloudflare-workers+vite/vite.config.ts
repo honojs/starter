6d758c0597003ed8f1128cded8ee4d2da9e720ea
oscarvz <oscarvz@users.noreply.github.com>
2025-03-31 12:11:24 +0200

    Add Hono build plugin to `cloudflare-workers+vite`

--- templates/cloudflare-workers+vite/vite.config.ts
+++ templates/cloudflare-workers+vite/vite.config.ts
@@ -1,7 +1,8 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
+import build from '@hono/vite-build/cloudflare-workers'
 import { defineConfig } from 'vite'
 import ssrHotReload from 'vite-plugin-ssr-hot-reload'
 
 export default defineConfig({
-  plugins: [ssrHotReload(), cloudflare()]
+  plugins: [ssrHotReload(), cloudflare(), build()],
 })
