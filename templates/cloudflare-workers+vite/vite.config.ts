e39685daeb557c6a1a922892be8a8cb9cf9a3f82
Yusuke Wada <yusuke@kamawada.com>
2025-05-28 07:23:37 +0900

    use `vite-ssr-components` for `cloudflare-workers+vite` (#96)

--- templates/cloudflare-workers+vite/vite.config.ts
+++ templates/cloudflare-workers+vite/vite.config.ts
@@ -1,25 +1,7 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
-import build from '@hono/vite-build/cloudflare-workers'
 import { defineConfig } from 'vite'
-import ssrHotReload from 'vite-plugin-ssr-hot-reload'
+import ssrPlugin from 'vite-ssr-components/plugin'
 
-export default defineConfig(({ command, isSsrBuild }) => {
-  if (command === 'serve') {
-    return { plugins: [ssrHotReload(), cloudflare()] }
-  }
-  if (!isSsrBuild) {
-    return {
-      build: {
-        rollupOptions: {
-          input: ['./src/style.css'],
-          output: {
-            assetFileNames: 'assets/[name].[ext]'
-          }
-        }
-      }
-    }
-  }
-  return {
-    plugins: [build({ outputDir: 'dist-server' })]
-  }
+export default defineConfig({
+  plugins: [cloudflare(), ssrPlugin()]
 })
