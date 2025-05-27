ed8043673d2b2716f2604fc1cd9aa78c1549823e
Yusuke Wada <yusuke@kamawada.com>
2025-05-28 07:21:50 +0900

    use `vite-ssr-components` for `cloudflare-workers+vite`

--- templates/cloudflare-workers+vite/src/renderer.tsx
+++ templates/cloudflare-workers+vite/src/renderer.tsx
@@ -1,13 +1,12 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
+import { Link, ViteClient } from 'vite-ssr-components/hono'
 
 export const renderer = jsxRenderer(({ children }) => {
   return (
     <html>
       <head>
-        <link
-          href={import.meta.env.PROD ? `/assets/style.css` : `/src/style.css`}
-          rel="stylesheet"
-        />
+        <ViteClient />
+        <Link href="/src/style.css" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
