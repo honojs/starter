33e1318c4212bee910d26d2e89545ac8db2a2fc1
Jeff See <jeffsee.55@gmail.com>
2025-07-16 17:32:26 -0700

    Update vercel template to user modern functions syntax

--- templates/vercel/api/edge.ts
+++ templates/vercel/api/edge.ts
@@ -1,6 +1,10 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
+export const config = {
+  runtime: 'edge'
+}
+
 const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
