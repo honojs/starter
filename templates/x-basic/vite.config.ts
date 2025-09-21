7065585604fafe2fe0297af7ca5256816d02f602
Moritz Eck <moritz.eck@gmail.com>
2025-09-21 22:15:04 +0200

    chore(x-basic): update client input paths in Vite configuration to reflect changes made in honox 0.1.47

--- templates/x-basic/vite.config.ts
+++ templates/x-basic/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     honox({
       devServer: { adapter },
-      client: { input: ['./app/style.css'] }
+      client: { input: ['/app/client.ts', '/app/style.css'] }
     }),
     tailwindcss(),
     build()
